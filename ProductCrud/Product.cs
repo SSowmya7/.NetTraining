8e2be03db6acd55f91837cda7fc6758b6fadcd2b
SSowmya7 <ssowmya@qualminds.com>
2024-04-30 17:38:14 +0530

    implemented crud using generics for product type

---
+++ ProductCrud/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPractice
+{
+    public class Product
+    {
+        public int ProdId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+       
+        public Product()
+        {
+
+        }
+        public Product(int prodId, string description, decimal price)
+        {
+            ProdId = prodId;
+            Description = description;
+            Price = price;
+            
+        }
+
+
+    }
+}
---
+++ ProductCrud/ProductUtility.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPractice
+{
+    public class ProductUtility
+    {
+        public static ProductOperations productsOperations = new ProductOperations();
+       
+
+        public void DoAdd()
+        {
+
+            Product obj = new Product();
+
+
+            Console.WriteLine("Enter the name of the Product");
+            obj.Description = Console.ReadLine();
+            Console.WriteLine("Enter the price of the product");
+            obj.Price = Convert.ToInt32(Console.ReadLine());
+          
+
+            productsOperations.AddProduct(obj);
+
+        }
+
+        public void DoDisplay()
+        {
+            List<Product> products = productsOperations.DisplayAllProducts();
+
+            foreach (Product prod in products)
+            {
+                Console.WriteLine("product id: " + prod.ProdId);
+                Console.WriteLine("product description: " + prod.Description);
+                Console.WriteLine("product price: " + prod.Price);
+            }
+        }
+
+        public void DoDelete()
+        {
+            Console.WriteLine("Enter the id of the product to be deleted");
+            int id = Convert.ToInt32(Console.ReadLine());
+            productsOperations.DeleteProduct(id);
+        }
+
+        public void DoUpdate()
+        {
+            Console.WriteLine("Enter the id of the product to updated");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+
+            Product obj = new Product();
+
+            Console.WriteLine("Enter the name of the product");
+            obj.Description = Console.ReadLine();
+            Console.WriteLine("Enter the price of the product");
+            obj.Price = Convert.ToInt32(Console.ReadLine());
+           
+
+            productsOperations.UpdateProduct(id, obj);
+
+
+        }
+    }
+}
