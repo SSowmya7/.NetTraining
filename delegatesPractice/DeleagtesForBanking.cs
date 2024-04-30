dabce47107b7f3ab5bf29e460ba17a213ff9722e
SSowmya7 <ssowmya@qualminds.com>
2024-04-30 17:03:33 +0530

    practices delegates for basic arithmetic operations and banking classes

---
+++ delegatesPractice/DeleagtesForBanking.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegatesPractice
+{
+    public class DeleagtesForBanking
+    {
+        public delegate void AccountHandler(decimal amount);
+        public class Account
+        {
+            private decimal _balance;
+            private string _name;
+            public Account(string name, decimal balance)
+            {
+                _name = name;
+                _balance = balance;
+            }
+            public void Deposit(decimal amountToDeposit)
+            {
+                try
+                {
+                    if (amountToDeposit > 0)
+                    {
+
+                        _balance += amountToDeposit;
+                        Console.WriteLine(_name, _balance);
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            public void WithDraw(decimal amountToWithDraw)
+            {
+                try
+                {
+                    if (amountToWithDraw > 0 && amountToWithDraw <= _balance)
+                    {
+                        _balance -= amountToWithDraw;
+
+                       Console.WriteLine(_name, _balance);
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                   
+                }
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Account Holder Name: {_name}\n" +
+               $"Account Balance: {_balance}");
+
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Account account = new Account("sowmya", 1000);
+
+                AccountHandler accObj1, accObj2, accObj3;
+
+                accObj1 = new AccountHandler(account.Deposit);
+               accObj2 = new AccountHandler(account.WithDraw);
+
+
+                
+
+                //Multicasting a delegate
+                accObj3 = accObj1 + accObj2;
+
+               
+
+                accObj1(200); // single casting
+
+                accObj2(3000); // single casting
+
+                accObj3(300); // multi casting
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ delegatesPractice1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace delegatesPractice
+{
+    
+       
+    public delegate void ArithmeticHandler(int firstNmuber,int secondNmuber);
+        public class Operations
+        {
+            public void Addition(int firstNumber, int secondNumber)
+            {
+                Console.WriteLine("Addtion of {0} and {1} is {2}", firstNumber, secondNumber, firstNumber + secondNumber);
+
+            }
+            public void Subtraction(int firstNumber, int secondNumber)
+            {
+                Console.WriteLine("Subtraction of {0} and {1} is {2}", firstNumber, secondNumber, firstNumber - secondNumber);
+
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                
+                Operations obj = new Operations();
+
+                ArithmeticHandler arithObj1, arithObj2, arithObj3;
+
+                arithObj1 = new ArithmeticHandler(obj.Addition);
+                arithObj2 = new ArithmeticHandler(obj.Subtraction);
+
+
+                ArithmeticHandler arithObj = new ArithmeticHandler(obj.Addition);
+
+                //Multicasting a delegate
+                arithObj3 = arithObj1 + arithObj2;
+                 
+                arithObj(1,2);
+
+                arithObj1(2, 3); // single casting
+
+                arithObj2(3, 2); // single casting
+
+                arithObj3(6, 4); // multi casting
+
+                Console.ReadKey();
+            }
+        }
+    }
+  
