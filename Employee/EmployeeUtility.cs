a739d726385a78b4efbeeb2ce7af0c4d033b134a
SSowmya7 <ssowmya@qualminds.com>
2024-04-30 17:07:33 +0530

    implemented crud for employee class using lists

---
+++ Employee/EmployeeUtility.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public  class EmployeeUtility
+    {
+        public static EmployeeOperations employeeOperations = new EmployeeOperations();
+      
+        public void DoAdd()
+        {
+
+            Employee obj = new Employee();
+
+
+            Console.WriteLine("Enter the name of the Employee");
+            obj.Name = Console.ReadLine();
+            Console.WriteLine("Enter the department of the Employee");
+            obj.Department = Console.ReadLine();
+            Console.WriteLine("Enter the salary of the Employee");
+            obj.Salary = Convert.ToDecimal(Console.ReadLine());
+
+            employeeOperations.AddEmployee(obj);
+
+        }
+
+        public  void DoDisplay()
+        {
+            ArrayList employees = employeeOperations.DisplayAllEmployees();
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine("employee Name: " + emp.Name);
+                Console.WriteLine("employee Department: " + emp.Department);
+                Console.WriteLine("employee Salary: " + emp.Salary);
+            }
+        }
+
+        public  void DoDelete()
+        {
+            Console.WriteLine("Enter the id of the Employee to be deleted");
+            int id = Convert.ToInt32(Console.ReadLine());
+            employeeOperations.DeleteEmployee(id);
+        }
+
+        public  void DoUpdate()
+        {
+            Console.WriteLine("Enter the id of the Employee to updated");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+
+            Employee obj = new Employee();
+
+            Console.WriteLine("Enter the Updated name of the Employee");
+            obj.Name = Console.ReadLine();
+            Console.WriteLine("Enter the Updated department of the Employee");
+            obj.Department = Console.ReadLine();
+            Console.WriteLine("Enter the Updated salary of the Employee");
+            obj.Salary = Convert.ToDecimal(Console.ReadLine());
+
+
+            employeeOperations.UpdateEmployee(id, obj);
+
+
+        }
+
+
+
+
+
+    }
+
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+
+namespace Employee
+{
+    public class Program
+    {
+       // static EmployeeOperations employeeOperations= new EmployeeOperations();
+        static EmployeeUtility employeeUtility= new EmployeeUtility();
+        public enum MenuOption
+        {
+            Add = 1,
+            Update,
+            DisplayEmployees,
+            Delete,
+            Exit
+        }
+        public static MenuOption ReadUserOption()
+        {
+           foreach (MenuOption option in Enum.GetValues(typeof(MenuOption)))
+           {
+
+                    Console.WriteLine($"{(int)option}. {option}");
+               
+            }
+            Console.Write("Enter your choice: ");
+            int options = Convert.ToInt32(Console.ReadLine());
+
+            return (MenuOption)options;
+
+
+        }
+       
+
+      
+       
+
+       
+        static void Main(string[] args)
+        {
+          
+            MenuOption choice = ReadUserOption();
+
+            while(choice != MenuOption.Exit) {
+                 switch (choice)
+                {
+                    case MenuOption.Add:
+
+                        employeeUtility.DoAdd();
+                        break;
+                     case MenuOption.Update:
+                        
+                       employeeUtility.DoUpdate();
+                      
+                        
+                        break;
+                    case MenuOption.DisplayEmployees:
+                        employeeUtility.DoDisplay();
+                        break;
+                    case MenuOption.Delete:
+                        employeeUtility.DoDelete();
+                        break;
+                    case MenuOption.Exit:
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+
+                        
+                }
+                choice = ReadUserOption();
+            }
+        }
+    }
+}
