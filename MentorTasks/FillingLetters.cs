d383a5201d458d9536709e901abb2948bf7ebaed
SSowmya7 <ssowmya@qualminds.com>
2024-05-02 12:40:37 +0530

    Done tasks on the given problem statements

---
+++ MentorTasks/FillingLetters.cs
@@ -0,0 +1,30 @@
+﻿namespace MentorTasks
+{
+
+    public class FillingLetters
+    {
+        static void PrintLettersInBetween(char start, char end)
+        {
+            if (start + 1 == end)
+            {
+                Console.WriteLine("No Letters in Between");
+                return;
+            }
+            for (char c = (char)(start + 1); c < end; c++)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the starting letter");
+            char start = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter the Ending Letter");
+          char end = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine($"letters between {start} and {end} are :");
+            PrintLettersInBetween(start,end);
+        }
+
+    }
+}
---
+++ MentorTasks/NumberToBits.cs
@@ -0,0 +1,24 @@
+﻿namespace MentorTasks
+{
+    public class NumberToBits
+    {
+        public static string NumToBits(int number)
+        {
+            string result = "";
+            while (number > 0)
+            {
+                int remainder = number % 2;
+                result = Convert.ToString(remainder) + result;
+                number /= 2;
+            }
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number to convert into bits");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(NumToBits(number));
+        }
+    }
+}
