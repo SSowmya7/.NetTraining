51e94f9bf6ef7091790a6c0de6a263f732753097
SSowmya7 <ssowmya@qualminds.com>
2024-05-08 12:48:21 +0530

    added changes for compressed strings program

---
+++ MentorTasks/CompressString.cs
@@ -0,0 +1,48 @@
+﻿namespace MentorTasks
+{
+    public class CompressString
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter string");
+            string str = Console.ReadLine();
+
+            string CompressedString = StringCompressor(str);
+            Console.WriteLine($"Compressed String = {CompressedString}");
+        }
+
+        public static string StringCompressor(string str)
+        {
+            int count;
+            int len = str.Length;
+            string CompressedString = "";
+            for (int i = 0;i<str.Length;)
+            {
+                char c = str[i];
+                count = CountOccurrences(str, c,i);
+                CompressedString = CompressedString + c + $"{count}";
+                
+                i += count;
+               
+
+            }
+            return CompressedString;
+        }
+        public static int CountOccurrences(string str, char target, int startIndex)
+        {
+            int count = 0;
+            for (int i = startIndex; i < str.Length; i++)
+            {
+                if (str[i] == target)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;  
+                }
+            }
+            return count;
+        }
+    }
+}
--- MentorTasks/FillingLetters.cs
+++ MentorTasks/FillingLetters.cs
@@ -13,18 +13,19 @@
             for (char c = (char)(start + 1); c < end; c++)
             {
                 Console.WriteLine(c);
-            }
+            } 
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the starting letter");
-            char start = Convert.ToChar(Console.ReadLine());
+            char start = Convert.ToChar(Console.ReadLine() ?? "a");
             Console.WriteLine("Enter the Ending Letter");
-          char end = Convert.ToChar(Console.ReadLine());
+          char end = Convert.ToChar(Console.ReadLine() ?? "a");
             Console.WriteLine($"letters between {start} and {end} are :");
             PrintLettersInBetween(start,end);
         }
 
+       
     }
 }
