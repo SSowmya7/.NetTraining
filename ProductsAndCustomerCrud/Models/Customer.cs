60fd074a428ff10b5646647034add9ffad0fa4a9
SSowmya7 <ssowmya@qualminds.com>
2024-05-21 18:36:04 +0530

    Practiced E.F with Product and customer Tables

---
+++ ProductsAndCustomerCrud/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFPractice.Models
+{
+    public class Customer
+    {
+
+        [Key] public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+    }
+}
