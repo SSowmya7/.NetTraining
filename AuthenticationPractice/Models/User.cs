d5585f42f66a3bfa1120768e2487f31ac8c1630a
SSowmya7 <ssowmya@qualminds.com>
2024-06-13 11:35:35 +0530

    deleted authentication practice example from authorisation branch

--- AuthenticationPractice/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthenticationPraction.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
